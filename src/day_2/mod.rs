f07a9cf769777f49341fac4261684eebc437b197
Noah Lundie <86733783+n-lundie@users.noreply.github.com>
2023-05-30 18:32:33 +0100

    refactor day_2 part_1

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -1,11 +1,13 @@
 use std::fs;
 
 mod part_1;
+mod part_2;
 
 pub fn run() {
-    let input = fs::read_to_string("src/day_2/inputs/actual").expect("Actual input should exist");
+    let input = fs::read("src/day_2/inputs/actual").expect("Actual input should exist");
 
     println!("Day 2:");
 
     println!("  Part 1: {}", part_1::sol(&input));
+    // println!("  Part 2: {}", part_2::sol(&input));
 }
--- src/day_2/part_1.rs
+++ src/day_2/part_1.rs
@@ -1,41 +1,9 @@
-pub fn sol(input: &String) -> u32 {
+pub fn sol(input: &[u8]) -> i16 {
     input
-        .lines()
-        .map(|l| {
-            let round = l.split(" ").collect::<Vec<&str>>();
-
-            match round[1] {
-                "X" => {
-                    let res = match round[0] {
-                        "B" => 0,
-                        "C" => 6,
-                        _ => 3,
-                    };
-
-                    res + 1
-                }
-                "Y" => {
-                    let res = match round[0] {
-                        "A" => 6,
-                        "C" => 0,
-                        _ => 3,
-                    };
-
-                    res + 2
-                }
-                "Z" => {
-                    let res = match round[0] {
-                        "A" => 0,
-                        "B" => 6,
-                        _ => 3,
-                    };
-
-                    res + 3
-                }
-                _ => panic!("Invalid input"),
-            }
-        })
-        .fold(0, |acc, e| acc + e)
+        .split(|b| *b == b'\n')
+        .map(|l| (l[0] as i16 - b'A' as i16, l[2] as i16 - b'X' as i16))
+        .map(|l| 1 + l.1 + 3 * (1 + l.1 - l.0).rem_euclid(3))
+        .sum()
 }
 
 #[cfg(test)]
@@ -44,9 +12,8 @@ mod tests {
     use std::fs;
     #[test]
     fn returns_expected_value_for_example() {
-        let input =
-            fs::read_to_string("src/day_2/inputs/test").expect("Example input should exist");
-        let expected_value = 15;
+        let input = fs::read("src/day_2/inputs/test").expect("Example input should exist");
+        let expected_value: i16 = 15;
 
         assert_eq!(sol(&input), expected_value);
     }
